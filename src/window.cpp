90ad15ddfe822e3fde6cc1b4b115e80ccc5ac17a
ABRIEL-Corentin <corentin.abriel@epitech.eu>
2023-06-01 10:48:11 +0200

    [FIX] Limit frame rate

--- src/window.cpp
+++ src/window.cpp
@@ -12,5 +12,7 @@ namespace PhysikEngine
 {
     Window::Window(const std::string &name, sf::Vector2u size)
         : sf::RenderWindow({size.x, size.y, 32}, name)
-    { }
+    {
+        setFramerateLimit(60);
+    }
 }
